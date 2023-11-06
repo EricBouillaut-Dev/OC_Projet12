26dbebf48c91dc61b861427480a047be87afac20
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-11-06 08:39:27 +0100

    Ajout balise <nav>

--- p12-sportsee/src/components/NavBar.js
+++ p12-sportsee/src/components/NavBar.js
@@ -2,16 +2,18 @@ import Logo from "../assets/logo.svg";
 
 function NavBar() {
   return (
-    <div className="navbar">
-      <img src={Logo} alt="Logo" />
-      <ul className="nav">
-        <li className="sportsee">SportSee</li>
-        <li>Accueil</li>
-        <li>Profil</li>
-        <li>Réglages</li>
-        <li>Communauté</li>
-      </ul>
-    </div>
+    <>
+      <nav className="navbar">
+        <img src={Logo} alt="Logo" />
+        <ul className="nav">
+          <li className="sportsee">SportSee</li>
+          <li>Accueil</li>
+          <li>Profil</li>
+          <li>Réglages</li>
+          <li>Communauté</li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
